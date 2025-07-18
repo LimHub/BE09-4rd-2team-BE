46c994518228423a7b6809b8725eafc6fdd60373
LimHub <dla6963@naver.com>
2025-07-18 17:18:41 +0900

    파이프라인구축최종테스트

--- blog-service/src/main/java/com/playblog/blogservice/neighbor/Controller/NeighborController.java
+++ blog-service/src/main/java/com/playblog/blogservice/neighbor/Controller/NeighborController.java
@@ -232,8 +232,6 @@ public class NeighborController {
 
         return ResponseEntity.ok(userInfo);
     }
-
-
     // 해당 유저정보 넘겨주기
     @GetMapping("/by-nickname/{nickname}")
     public ResponseEntity<insertPopup> getUserByNickname(
